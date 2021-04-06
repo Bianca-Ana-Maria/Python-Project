e720832b45ea4c821a1a7c65c77f883657b387cb
Bianca-Ana-Maria <81956136+Bianca-Ana-Maria@users.noreply.github.com>
2021-04-06 16:19:08 +0300

    Add Output

--- python-2.py
+++ python-2.py
@@ -2,4 +2,11 @@ while True:
     number = float(input('Enter a number: '))
     if number < 0:
         break
-    print('You entered: ', number)
+    print('You entered: ', number) 
+    
+    #Output
+Enter a number: 5.2
+You entered:  5.2
+Enter a number: -9 
+>
+        
